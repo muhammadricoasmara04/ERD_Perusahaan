d68123f723eb980f9335ff625893c63f88ca54c9
muhammadricoasmara04 <ricoasmara496@gmail.com>
2023-12-28 20:04:18 +0700

    viewmodel register and login on going

--- app/src/main/java/com/example/erd_perusahaan/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/erd_perusahaan/ui/login/LoginViewModel.kt
@@ -2,9 +2,14 @@ package com.example.erd_perusahaan.ui.login
 
 import android.app.Application
 import androidx.databinding.Bindable
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.erd_perusahaan.database.RegisterRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class LoginViewModel(private val repository: RegisterRepository,application: Application):
     ViewModel() {
@@ -12,12 +17,56 @@ class LoginViewModel(private val repository: RegisterRepository,application: App
 
 
     @Bindable
-    val inputUsername= MutableLiveData<String>()
+    val inputEmail= MutableLiveData<String>()
 
     @Bindable
     val inputPassword=MutableLiveData<String>()
 
-    fun loginButton(){
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
+    private val _navigatetoRegister = MutableLiveData<Boolean>()
+    val navigatetoRegister: LiveData<Boolean>
+        get() = _navigatetoRegister
+
+    private val _navigatetoUserDetails = MutableLiveData<Boolean>()
+
+    val navigatetoUserDetails: LiveData<Boolean>
+        get() = _navigatetoUserDetails
+
+    private val _errorToast = MutableLiveData<Boolean>()
+
+    val errotoast: LiveData<Boolean>
+        get() = _errorToast
+
+    private val _errorToastUsername = MutableLiveData<Boolean>()
+
+    val errotoastUsername: LiveData<Boolean>
+        get() = _errorToastUsername
+
+    private val _errorToastInvalidPassword = MutableLiveData<Boolean>()
+
+    val errorToastInvalidPassword: LiveData<Boolean>
+        get() = _errorToastInvalidPassword
+
+
+    fun signUP() {
+        _navigatetoRegister.value = true
+    }
+
+    fun loginButton(){
+    if(inputEmail.value==null||inputPassword.value==null){
+        _errorToast.value=true
+    }else{
+        uiScope.launch {
+            val userName=repository.getUsername(inputEmail.value!!)
+            if (userName!=null){
+                if (userName.password==inputPassword.value){
+                    inputEmail.value = null
+
+                }
+            }
+        }
+    }
     }
     }
--- app/src/main/java/com/example/erd_perusahaan/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/erd_perusahaan/ui/register/RegisterViewModel.kt
@@ -1,40 +1,95 @@
 package com.example.erd_perusahaan.ui.register
 
 import android.app.Application
+import android.util.Log
 import androidx.databinding.Bindable
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.erd_perusahaan.database.RegisterEntity
 import com.example.erd_perusahaan.database.RegisterRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class RegisterViewModel(private val repository: RegisterRepository,private val application: Application): ViewModel() {
-    @Bindable
-    val inputUsername= MutableLiveData<String>()
+
+    private var userdata: String? = null
+
+    var userDetailsLiveData = MutableLiveData<Array<RegisterEntity>>()
+
 
     @Bindable
-    val inputPassword= MutableLiveData<String>()
-     @Bindable
-    val inputEmail= MutableLiveData<String>()
+    val inputUsername = MutableLiveData<String>()
+
+    @Bindable
+    val inputPassword = MutableLiveData<String>()
+
+    @Bindable
+    val inputEmail = MutableLiveData<String>()
+
+    private val _navigateto = MutableLiveData<Boolean>()
+    val navigateto: LiveData<Boolean>
+        get() = _navigateto
+
+    private val _errorToast = MutableLiveData<Boolean>()
+
+    val errotoast: LiveData<Boolean>
+        get() = _errorToast
+
+    private val _errorToastUsername = MutableLiveData<Boolean>()
+
+    val errotoastUsername: LiveData<Boolean>
+        get() = _errorToastUsername
+
     private val viewModelJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
 
-
-    suspend fun sumbitButton(){
-        if (inputUsername.value==null ||inputEmail.value==null||inputPassword==null){
-
-    }else{
+    suspend fun sumbitButton() {
+        Log.i("MyTag", "Inside Submit Button")
+        if (inputUsername.value == null || inputEmail.value == null || inputPassword == null) {
+            _errorToast.value = true
+        } else {
+            uiScope.launch {
+                val userName= repository.getUsername(inputUsername.value!!)
+                Log.i("MYtag",userName.toString()+"--------------")
+                if (userName!=null){
+                    _errorToastUsername.value= true
+                    Log.i("MYTag","Inside if Not null")
+                }else{
+                    Log.i("MYTAG", userDetailsLiveData.value.toString() + "ASDFASDFASDFASDF")
+                    Log.i("MYTAG", "OK im in")
+                    val userName=inputUsername.value!!
+                    val userEmail=inputEmail.value!!
+                    val userPassword=inputPassword.value!!
+                    _navigateto.value=true
+                }
+            }
         }
-        val userName=repository.getUsername(inputUsername.value!!)
-        if (userName!=null){
-        }else{
-            val username=inputUsername.value!!
-            val email=inputUsername.value!!
-            val password=inputPassword.value!!
+    }
+    override fun onCleared() {
+        super.onCleared()
+    }
 
-        }
+    fun doneNavigating() {
+        _navigateto.value = false
+        Log.i("MYTAG", "Done navigating ")
+    }
+
+    fun donetoast() {
+        _errorToast.value = false
+        Log.i("MYTAG", "Done taoasting ")
+    }
+
+    fun donetoastUserName() {
+        _errorToast.value = false
+        Log.i("MYTAG", "Done taoasting  username")
+    }
+    private fun insert(user:RegisterEntity):Job=viewModelScope.launch {
+        repository.insert(user)
     }
 }
